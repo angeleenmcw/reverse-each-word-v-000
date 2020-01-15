b75964863821cbde999ad9af22c814a4cbf59827
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-15 08:38:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1 = "Hello I am Angeleen"
 sentence2 = "What are you doing here?"
-sentences = [sentence_1, sentence_2]
+sentences = [sentence1, sentence2]
 def reverse_each_word(sentences)
   words = sentences.split
   words_reverse = words.each do |word|
