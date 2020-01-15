f38ba312fa61ce7995bfd0a966ab642a06d2bcfc
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-15 08:38:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence2 = "What are you doing here?"
 sentences = [sentence1, sentence2]
 def reverse_each_word(sentences)
   words = sentences.split
-  words_reverse = words.each do |word|
+  words_reverse = words.collect do |word|
     word.reverse
   end
   words_reverse.join(" ")
