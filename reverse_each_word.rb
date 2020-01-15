d59878569cc412d116401c35af782197c3543027
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-15 08:36:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,4 +3,5 @@ sentence_2 = "What are you doing here?"
 sentences = [sentence_1, sentence_2]
 def reverse_each_word(sentences)
   words = sentences.split
-  words_reverse = sentences
+  words_reverse = words.each do |word|
+    
